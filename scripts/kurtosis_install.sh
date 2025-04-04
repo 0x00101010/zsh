862d5f49bd3897ebf942a06f57a602656b1b2fd8
Francis Li <lihuiyi0406@gmail.com>
2025-04-04 16:53:51 -0700

    Update install kurtosis script to detect os

--- scripts/kurtosis_install.sh
+++ scripts/kurtosis_install.sh
@@ -1,3 +1,13 @@
-echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
-sudo apt update
-sudo apt install kurtosis-cli
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Assume it's a Debian/Ubuntu-based Linux
+    echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
+    sudo apt update
+    sudo apt install kurtosis-cli
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    echo "Detected macOS"
+    brew install kurtosis-tech/tap/kurtosis-cli
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
